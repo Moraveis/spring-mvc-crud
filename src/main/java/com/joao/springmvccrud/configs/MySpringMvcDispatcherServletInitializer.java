4c06206d89e3385b9a84f99db249269c656a7010
Joao Vitor <joaovitormoraveis@gmail.com>
2022-12-28 20:32:41 -0500

    Project setup

---
+++ src/main/java/com/joao/springmvccrud/configs/MySpringMvcDispatcherServletInitializer.java
@@ -0,0 +1,35 @@
+package com.joao.springmvccrud.configs;
+
+import org.h2.server.web.WebServlet;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+
+public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        super.onStartup(servletContext);
+        ServletRegistration.Dynamic servlet = servletContext.addServlet("h2-console", new WebServlet());
+        servlet.setLoadOnStartup(2);
+        servlet.addMapping("/console/*");
+    }
+
+}
