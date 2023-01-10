17a2291819463cb60940270945dd144582e7a09c
Joao Vitor <joaovitormoraveis@gmail.com>
2023-01-09 20:17:15 -0500

    Adding support for AspectJ for logging

--- src/main/java/com/joao/springmvccrud/configs/SpringWebConfig.java
+++ src/main/java/com/joao/springmvccrud/configs/SpringWebConfig.java
@@ -3,6 +3,7 @@ package com.joao.springmvccrud.configs;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -13,6 +14,7 @@ import org.springframework.web.servlet.view.JstlView;
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = {"com.joao.springmvccrud"})
+@EnableAspectJAutoProxy
 public class SpringWebConfig implements WebMvcConfigurer {
 
     @Override
